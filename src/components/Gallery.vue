9bcb67e49d6907328500741827d483a936892c6d
Equipoise <equipoise7@yandex.ru>
2025-11-13 13:34:11 +0300

    Сделан оверлей карточек маршрутов более светлым

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -253,13 +253,13 @@ const closeImage = () => {
   justify-content: center;
   align-items: center;
   padding: 2rem;
-  background: rgba(0, 0, 0, 0.15);
+  background: rgba(0, 0, 0, 0.05);
   color: white;
   transition: all 0.4s ease;
 }
 
 .gallery-item:hover .gallery-overlay {
-  background: rgba(0, 0, 0, 0.5);
+  background: rgba(0, 0, 0, 0.3);
 }
 
 .gallery-title {
